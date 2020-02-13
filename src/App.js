40a8855d484f7de99b3e59e3380bdf8777b2fdd1
ssearobber <ssearobber@naver.com>
2020-02-13 19:40:25 +0900

    20200213

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const position = [51.505, -0.09];
+    const map = L.map('map').setView(position, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(position)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.');
+  }
+
+  render() {
+    return <div className="App" id="map"></div>;
+  }
 }
 
 export default App;
